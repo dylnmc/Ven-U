6c011ac01f1a1b7a366000b24c01f8d6260079f7
dylnmc <dylnmc@gmail.com>
2019-11-10 16:57:31 -0500

    Init commit

---
+++ app/src/main/java/edu/fsu/cs/ven_u/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.fsu.cs.ven_u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
