c385d2e8d9e8c4b9d4c140158b1bf5a96e8c61f4
dylnmc <dylnmc@gmail.com>
2019-11-14 17:58:18 -0500

    adds simple CardView for Timeline
    Per this tutorial <https://www.youtube.com/watch?v=17NbUcEts9c>, I was able to get a rudimentary
    CardView working.
    
    The next steps for the Timeline are:
    
    1. Integrate with the BottomNavigationView that Bianca made
    2. Get firebase Events from Kenny
        - Where do we put these initially? Local db since firebase async?
        - Load all downloaded Events into the CardView based
    3. Provide event search
    4. Separate private, protected, and public events with a divider

--- app/src/main/java/edu/fsu/cs/ven_u/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/ven_u/MainActivity.java
@@ -1,14 +1,34 @@
 package edu.fsu.cs.ven_u;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<TimelineItem> timelineList = new ArrayList<>();
+        timelineList.add(new TimelineItem("Gina's Party", "public"));
+        timelineList.add(new TimelineItem("RubiCorp Symposium", "public"));
+        timelineList.add(new TimelineItem("Bob's Party", "public"));
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new TimelineAdapter(timelineList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
